ed5ab82a37e7f802c9ff4dd7f710df7351a2c8d2
Alexandre Sobota-Linux <alexandre.sobota@hotmail.com>
2021-07-12 15:53:20 -0300

    Fourth class

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -1,32 +1,34 @@
-const Job = require('../model/Job');
-const JobUtils = require('../utils/JobUtils');
-const Profile = require('../model/Profile');
+const Job = require("../model/Job");
+const JobUtils = require("../utils/JobUtils");
+const Profile = require("../model/Profile");
 
 module.exports = {
-  index(req, res) {
-
-    const jobs = Job.get();
-    const profile = Profile.get();
+  async index(req, res) {
+    const jobs = await Job.get();
+    const profile = await Profile.get();
 
     let statusCount = {
       progress: 0,
       done: 0,
-      total: jobs.length
-    }
+      total: jobs.length,
+    };
 
     // total de horas por dia de cada Job em progresso
-    let jobTotalHours = 0
+    let jobTotalHours = 0;
 
     const updatedJobs = jobs.map((job) => {
       // ajustes no job
-      const remaining = JobUtils.remainingDays(job)
-      const status = remaining <= 0 ? 'done' : 'progress'
-  
+      const remaining = JobUtils.remainingDays(job);
+      const status = remaining <= 0 ? "done" : "progress";
+
       // somando a quantidade de status
       statusCount[status] += 1;
 
       // total de horas por dia de cada Job em progresso
-      jobTotalHours = status === 'progress' ? jobTotalHours + Number(job['daily-hours']) : jobTotalHours
+      jobTotalHours =
+        status === "progress"
+          ? jobTotalHours + Number(job["daily-hours"])
+          : jobTotalHours;
 
       return {
         ...job,
@@ -40,7 +42,12 @@ module.exports = {
     // MENOS
     // a quantidade de horas/dia de cada job em progress
     const freeHours = profile["hours-per-day"] - jobTotalHours;
-  
-    return res.render("index", { jobs: updatedJobs, profile: profile, statusCount: statusCount, freeHours: freeHours })
+
+    return res.render("index", {
+      jobs: updatedJobs,
+      profile: profile,
+      statusCount: statusCount,
+      freeHours: freeHours,
+    });
   },
 };
--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -1,78 +1,60 @@
-const Job = require('../model/Job');
-const JobUtils = require('../utils/JobUtils');
-const Profile = require('../model/Profile');
+const Job = require("../model/Job");
+const JobUtils = require("../utils/JobUtils");
+const Profile = require("../model/Profile");
 
 module.exports = {
   create(req, res) {
-    return res.render("job")
+    return res.render("job");
   },
 
-  save(req, res) {
+  async save(req, res) {
     // req.body = { name: 'olaakaa', 'daily-hours': '1', 'total-hours': '3' }
-    const jobs = Job.get()
-    const lastId = jobs[jobs.length - 1]?.id || 0;
 
-    jobs.push({
-      id: lastId + 1,
+    await Job.create({
       name: req.body.name,
       "daily-hours": req.body["daily-hours"],
       "total-hours": req.body["total-hours"],
-      created_at: Date.now() // Atribuindo data de hoje
-    })
-    return res.redirect('/')
+      created_at: Date.now(), // Atribuindo data de hoje
+    });
+
+    return res.redirect("/");
   },
 
-  show(req, res) {
-    const jobId = req.params.id
-    const jobs = Job.get()
-    const profile = Profile.get()
+  async show(req, res) {
+    const jobId = req.params.id;
+    const jobs = await Job.get();
+    const profile = await Profile.get();
 
-    const job = jobs.find(job => Number(job.id) === Number(jobId))
+    const job = jobs.find((job) => Number(job.id) === Number(jobId));
 
-    if(!job) {
-      return res.send('Job not found!')
+    if (!job) {
+      return res.send("Job not found!");
     }
 
-    job.budget = JobUtils.calculateBudget(job, profile["value-hour"])
+    job.budget = JobUtils.calculateBudget(job, profile["value-hour"]);
 
-    return res.render("job-edit", { job })
+    return res.render("job-edit", { job });
   },
 
-  update(req, res) {
-    const jobId = req.params.id
-    const jobs = Job.get()
-
-    const job = jobs.find(job => Number(job.id) === Number(jobId))
-
-    if(!job) {
-      return res.send('Job not found!')
-    }
+  async update(req, res) {
+    const jobId = req.params.id;
 
     const updatedJob = {
-      ...job,
       name: req.body.name,
       "total-hours": req.body["total-hours"],
       "daily-hours": req.body["daily-hours"],
-    }
+    };
 
-     const newJobs = jobs.map(job => {
-      if(Number(job.id) === Number(jobId)) {
-        job = updatedJob
-      }
+    await Job.update(updatedJob, jobId);
 
-      return job
-    })
-
-    Job.update(newJobs)
-
-    res.redirect("/job/" + jobId)
+    res.redirect("/job/" + jobId);
   },
 
-  delete(req, res) {
-    const jobId = req.params.id
+  async delete(req, res) {
+    const jobId = req.params.id;
 
-    Job.delete(jobId)
+    await Job.delete(jobId);
 
-    return res.redirect('/')
+    return res.redirect("/");
   },
-}
+};
